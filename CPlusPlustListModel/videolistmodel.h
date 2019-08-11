70272a57876b621c21d54290282170473888b489
rownh <799698579@qq.com>
2019-08-11 21:45:36 +0800

    xmlhttp_get

---
+++ CPlusPlustListModel/videolistmodel.h
@@ -0,0 +1,12 @@
+#ifndef VIDEOLISTMODEL_H
+#define VIDEOLISTMODEL_H
+
+#include <QObject>
+
+class VideoListModel : public QAbstractListModel
+{
+public:
+    VideoListModel();
+};
+
+#endif // VIDEOLISTMODEL_H
